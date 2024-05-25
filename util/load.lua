266f85dd4a557aee42b695c5b5a84b0fd05fb976
em <154848349+emsoftworks@users.noreply.github.com>
2024-05-25 14:02:50 -0400

    Update load.lua

--- util/load.lua
+++ util/load.lua
@@ -1,5 +1,5 @@
 local utils = {
-    CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/CTween.lua"))(),
-    PathFind = loadstring(game:HttpGet("https://raw.githubusercontent.com/Bovanlaarhoven/Util/main/Pathfinding.lua"))()
+    CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/emsoftworks/kankanware/main/util/CTween.lua"))(),
+    PathFind = loadstring(game:HttpGet("https://raw.githubusercontent.com/emsoftworks/kankanware/main/util/Pathfinding.lua"))()
 }
 return utils
